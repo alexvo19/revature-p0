86981ae796092be6d9cde4dcddea33419d28eda2
alex <alex>
2019-04-25 16:18:44 -0500

    fix linux-setup

--- linux-setup.sh
+++ linux-setup.sh
@@ -3,7 +3,7 @@
 #ubuntu
 apt update
 apt upgrade -y
-apt-get install -y build-essential curl file git #build-essential= group of packages
+apt-get install -y build-essential curl file git
 
 echo 'completed ubuntu section'
 
@@ -12,6 +12,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/i
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+eval \$($(brew --prefix)/bin/brew shellenv)
 
 echo 'completed brew section'
 
